0e8eceb58f43c5ec53ac105a78cd619bb5db473f
cbdowell <cbdowell@gmail.com>
2018-07-23 03:27:10 -0700

    Initial Files

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,4 +1,4 @@
-import { awesomeFn } from '@quramy/x-core'
+import { awesomeFn } from '@microbx/core'
 
 export function cli() {
     awesomeFn()
--- packages/x-core/lib/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function awesomeFn() {
-    console.log("Hello");
-}
-exports.awesomeFn = awesomeFn;
-//# sourceMappingURL=index.js.map
