e0248835b2bb9cbb460e15c7b4c492f23079f3a6
cbdowell <cbdowell@gmail.com>
2018-07-23 03:12:41 -0700

    Initial Files

---
+++ packages/x-cli/lib/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const x_core_1 = require("@quramy/x-core");
+function cli() {
+    x_core_1.awesomeFn();
+    return Promise.resolve(true);
+}
+exports.cli = cli;
+//# sourceMappingURL=main.js.map
---
+++ packages/x-core/lib/index.d.ts
@@ -0,0 +1,1 @@
+export declare function awesomeFn(): void;
